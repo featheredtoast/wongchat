e514484fdf8ccc8a947d62eb2489f6d58e5e8750
Jeff Wong <awole20@gmail.com>
2016-11-24 12:10:03 -0800

    refactor - sente and amqp events split out

---
+++ src/clj/sente_websockets_rabbitmq/events.clj
@@ -0,0 +1,86 @@
+(ns sente-websockets-rabbitmq.events
+  (:require
+   [langohr.queue     :as lq]
+   [langohr.consumers :as lc]
+   [langohr.basic     :as lb]
+   [clojure.data.json :as json]
+   [taoensso.sente :as sente]
+   [com.stuartsierra.component :as component]
+   [sente-websockets-rabbitmq.config :refer [get-property]]
+   [sente-websockets-rabbitmq.db :as db]))
+
+(defn rabbitmq-config []
+  (let [host (get-property :amqp-host "localhost")
+        port (get-property :amqp-port 5672)
+        username (get-property :amqp-user "guest")
+        password (get-property :amqp-pass "guest")
+        uri (str "amqp://" username ":" password "@" host ":" port)
+        final-uri (or (get-property :rabbitmq-bigwig-rx-url nil) uri)]
+    (println "amqp uri " final-uri)
+    final-uri))
+
+(defmulti event-msg-handler (fn [_ msg] (:id msg))) ; Dispatch on event-id
+;; Wrap for logging, catching, etc.:
+(defn     event-msg-handler* [rabbit-data {:as ev-msg :keys [id ?data event]}]
+  (event-msg-handler rabbit-data ev-msg))
+(do ; Client-side methods
+  (defmethod event-msg-handler :default ; Fallback
+    [_ {:as ev-msg :keys [event]}]
+    (println "Unhandled event: %s" event))
+  
+  (defmethod event-msg-handler :chsk/ws-ping
+    [_ {:as ev-msg :keys [event]}])
+
+  (defmethod event-msg-handler :chsk/uidport-open
+    [_ {:as ev-msg :keys [uid]}]
+    (println "new client connection"))
+
+  (defmethod event-msg-handler :chat/init
+    [_ {:as ev-msg :keys [?data uid send-fn]}]
+    (println "init: " uid)
+    (send-fn uid
+             [:chat/init
+              (reverse (vec (db/get-recent-messages)))]))
+
+  (defmethod event-msg-handler :chat/submit
+    [rabbit-data {:as ev-msg :keys [?data uid]}]
+    (let [msg (:msg ?data)]
+      (println "Event from " uid ": " msg)
+      (db/insert-message uid msg)
+      (lb/publish (:ch rabbit-data) "" (:qname rabbit-data) (json/write-str {:msg msg :uid uid}) {:content-type "text/json" :type "greetings.hi"}))))
+
+(defn sente-handler [{:keys [rabbit-mq]}]
+  (let [{:keys [ch]} rabbit-mq]
+    (partial event-msg-handler* {:ch ch :qname "hello"})))
+
+(defn message-handler
+  [chsk-send! connected-uids
+   ch {:keys [content-type delivery-tag type] :as meta} ^bytes payload]
+  (let [payload (json/read-str (String. payload "UTF-8")
+                               :key-fn keyword)
+        msg (:msg payload)
+        sender-uid (:uid payload)]
+    (println "Broadcasting server>user: %s" @connected-uids)
+    (println "sending: " payload)
+    (doseq [uid (:any @connected-uids)]
+      (chsk-send! uid
+                  [:chat/message
+                   {:msg msg
+                    :uid sender-uid}]))))
+
+(defrecord Messager [sente rabbit-mq]
+  component/Lifecycle
+  (start [component]
+    (let [qname "hello"
+          {:keys [chsk-send! connected-uids]} sente
+          {:keys [ch]} rabbit-mq]
+      (println (format "[main] Connected. Channel id: %d" (.getChannelNumber ch)))
+      (lq/declare ch qname {:exclusive false :auto-delete false})
+      (lc/subscribe ch qname (partial message-handler chsk-send! connected-uids) {:auto-ack true})
+      component))
+  (stop [component]
+    component))
+
+(defn new-messager []
+  (map->Messager {}))
+
--- src/clj/sente_websockets_rabbitmq/server.clj
+++ src/clj/sente_websockets_rabbitmq/server.clj
@@ -21,64 +21,15 @@
             [clojure.core.async :as async :refer (<! <!! >! >!! put! take! chan go go-loop)]
             [taoensso.sente :as sente]
             [taoensso.sente.server-adapters.http-kit      :refer (sente-web-server-adapter)]
-            [langohr.core      :as rmq]
-            [langohr.channel   :as lch]
-            [langohr.queue     :as lq]
-            [langohr.consumers :as lc]
-            [langohr.basic     :as lb]
             [cemerick.friend :as friend]
-            [clojure.data.json :as json]
             [clj-redis-session.core :as redis-session]
             [sente-websockets-rabbitmq.config :refer [get-property]]
             [sente-websockets-rabbitmq.db :as db]
+            [sente-websockets-rabbitmq.events :as events]
             [sente-websockets-rabbitmq.auth :as auth])
   (:gen-class))
 
 (def redis-conn {:spec {:uri (get-property :redis-url "redis://user:pass@localhost:6379")}})
-
-(defn rabbitmq-config []
-  (let [host (get-property :amqp-host "localhost")
-        port (get-property :amqp-port 5672)
-        username (get-property :amqp-user "guest")
-        password (get-property :amqp-pass "guest")
-        uri (str "amqp://" username ":" password "@" host ":" port)
-        final-uri (or (get-property :rabbitmq-bigwig-rx-url nil) uri)]
-    (println "amqp uri " final-uri)
-    final-uri))
-
-(defmulti event-msg-handler (fn [_ msg] (:id msg))) ; Dispatch on event-id
-;; Wrap for logging, catching, etc.:
-(defn     event-msg-handler* [rabbit-data {:as ev-msg :keys [id ?data event]}]
-  (event-msg-handler rabbit-data ev-msg))
-(do ; Client-side methods
-  (defmethod event-msg-handler :default ; Fallback
-    [_ {:as ev-msg :keys [event]}]
-    (println "Unhandled event: %s" event))
-  
-  (defmethod event-msg-handler :chsk/ws-ping
-    [_ {:as ev-msg :keys [event]}])
-
-  (defmethod event-msg-handler :chsk/uidport-open
-    [_ {:as ev-msg :keys [uid]}]
-    (println "new client connection"))
-
-  (defmethod event-msg-handler :chat/init
-    [_ {:as ev-msg :keys [?data uid send-fn]}]
-    (println "init: " uid)
-    (send-fn uid
-             [:chat/init
-              (reverse (vec (db/get-recent-messages)))]))
-
-  (defmethod event-msg-handler :chat/submit
-    [rabbit-data {:as ev-msg :keys [?data uid]}]
-    (let [msg (:msg ?data)]
-      (println "Event from " uid ": " msg)
-      (db/insert-message uid msg)
-      (lb/publish (:ch rabbit-data) "" (:qname rabbit-data) (json/write-str {:msg msg :uid uid}) {:content-type "text/json" :type "greetings.hi"}))))
-
-(defn sente-handler [{:keys [rabbit-mq]}]
-  (let [{:keys [ch]} rabbit-mq]
-    (partial event-msg-handler* {:ch ch :qname "hello"})))
 
 (defroutes routes
   (GET "/" _
@@ -102,46 +53,19 @@
        {:workflows [auth/workflow] :auth-url "/login"
         :credential-fn auth/credential-fn})))
 
-(defn message-handler
-  [chsk-send! connected-uids
-   ch {:keys [content-type delivery-tag type] :as meta} ^bytes payload]
-  (let [payload (json/read-str (String. payload "UTF-8")
-                               :key-fn keyword)
-        msg (:msg payload)
-        sender-uid (:uid payload)]
-    (println "Broadcasting server>user: %s" @connected-uids)
-    (println "sending: " payload)
-    (doseq [uid (:any @connected-uids)]
-      (chsk-send! uid
-                  [:chat/message
-                   {:msg msg
-                    :uid sender-uid}]))))
-
-(defrecord Messager [sente rabbit-mq]
-  component/Lifecycle
-  (start [component]
-    (let [qname "hello"
-          {:keys [chsk-send! connected-uids]} sente
-          {:keys [ch]} rabbit-mq]
-      (println (format "[main] Connected. Channel id: %d" (.getChannelNumber ch)))
-      (lq/declare ch qname {:exclusive false :auto-delete false})
-      (lc/subscribe ch qname (partial message-handler chsk-send! connected-uids) {:auto-ack true})
-      component))
-  (stop [component]
-    component))
-(defn new-messager []
-  (map->Messager {}))
-
 (defn prod-system []
   (component/system-map
    :db-migrate (db/new-migrate)
-   :rabbit-mq (new-rabbit-mq (rabbitmq-config))
+   :rabbit-mq (new-rabbit-mq (events/rabbitmq-config))
    :sente (component/using
-           (new-channel-sockets sente-handler sente-web-server-adapter {:wrap-component? true
-                                                                        :user-id-fn auth/get-user-id})
+           (new-channel-sockets
+            events/sente-handler
+            sente-web-server-adapter
+            {:wrap-component? true
+             :user-id-fn auth/get-user-id})
            [:rabbit-mq])
    :post-handler (component/using
-                  (new-messager)
+                  (events/new-messager)
                   [:sente :rabbit-mq])
    :sente-endpoint (component/using
                     (new-endpoint sente-routes)
@@ -149,8 +73,7 @@
    :routes (new-endpoint get-http-handler)
    :middleware (new-middleware  {:middleware [[wrap-defaults :defaults]
                                               wrap-with-logger
-                                              wrap-gzip
-                                              wrap-reload]
+                                              wrap-gzip]
                                  :defaults (assoc site-defaults :session {:store (redis-session/redis-store redis-conn)})})
    :handler (component/using
              (new-handler)
