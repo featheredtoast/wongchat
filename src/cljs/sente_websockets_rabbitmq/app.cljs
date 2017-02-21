b7823e1a705e1df24b79ba5a6a0a6d79831125a5
Jeff Wong <awole20@gmail.com>
2017-02-20 23:32:47 -0800

    sync finger distance

--- src/cljs/sente_websockets_rabbitmq/app.cljs
+++ src/cljs/sente_websockets_rabbitmq/app.cljs
@@ -226,7 +226,7 @@
                               :velocity velocity
                               :distance delta-distance
                               :end? false})
-    (swap! app-state assoc-in [:menu :percent-open] (min 100 (+ (* (/ 4 201) delta-distance) (get-in @app-state [:menu :percent-open]))))))
+    (swap! app-state assoc-in [:menu :percent-open] (min 100 (+ (* (/ 2 201) delta-distance) (get-in @app-state [:menu :percent-open]))))))
 
 (defn swipe-close-menu [velocity distance]
   (let [delta-distance (.abs js/Math (- distance (:distance @last-swipe-event)))]
@@ -234,7 +234,7 @@
                               :velocity velocity
                               :distance delta-distance
                               :end? false})
-    (swap! app-state assoc-in [:menu :percent-open] (max 0 (- (get-in @app-state [:menu :percent-open]) (* (/ 4 201) delta-distance))))))
+    (swap! app-state assoc-in [:menu :percent-open] (max 0 (- (get-in @app-state [:menu :percent-open]) (* (/ 2 201) delta-distance))))))
 
 (defn swipe-end []
   (let [{:keys [direction velocity distance]} @last-swipe-event
