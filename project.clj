d7f2ea6b454b8e313d6df2863e71e28822b0de1b
Jeff Wong <awole20@gmail.com>
2017-08-14 10:39:21 -0700

    bump clojure version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.854" :scope "provided"]
                  [http-kit "2.2.0"]
                  [ring "1.6.2"]
